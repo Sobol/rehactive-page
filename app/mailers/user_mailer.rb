028ccdcabeeeb8d54bf53638e2fee0869a87708d
Paweł Sobolewski <pawel.sobolewski@amberbit.com>
2012-04-22 16:14:57 +0200

    email configurationm

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
   def contact(contact)
     @contact = contact
     mail({
-     to: "pawel.sobolewski@amberbit.com" ,
+     to: "rehactive@gmail.com" ,
      subject: @contact.name + " zkontaktował się z tobą ze strony www.rehactivce.com",
      from: @contact.email,
      reply_to: @contact.email
