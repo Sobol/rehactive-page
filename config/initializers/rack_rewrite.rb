d7b108becb78ce799b94f329b0a99610a1a17686
Paweł Sobolewski <pawel.sobolewski@amberbit.com>
2012-05-10 23:37:31 +0200

    Smaller images

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,7 +1,5 @@
 RehactivePage::Application.config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
-  unless Rails.env.development?
-    r301 %r{.*}, 'http://www.rehactive.pl$&', :if => Proc.new {|rack_env|
-      rack_env['SERVER_NAME'] == 'rehactive.pl'
-    }
-  end
+  r301 %r{.*}, 'http://www.rehactive.pl$&', :if => Proc.new {|rack_env|
+    rack_env['SERVER_NAME'] == 'rehactive.pl'
+  }
 end
