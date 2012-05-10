52cd3f0c055987a341ef6f07a0ac5688be5baffb
Paweł Sobolewski <pawel.sobolewski@amberbit.com>
2012-05-10 23:58:15 +0200

    Rack rewrite last home setup

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,5 +1,5 @@
 RehactivePage::Application.config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
   r301 %r{.*}, 'http://www.rehactive.pl$&', :if => Proc.new {|rack_env|
-    rack_env['SERVER_NAME'] != 'www.rehactive.pl'
+    rack_env['SERVER_NAME'] == 'www.rehactive.pl'
   }
 end
