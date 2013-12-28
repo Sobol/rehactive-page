5aa97bf371fcc4ccdce78a7c970d3d3a9c81f96d
Paweł Sobolewski <pawel.sobolewski@amberbit.com>
2013-12-28 19:14:37 +0100

    News in proper order

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,7 +1,7 @@
 class NewsController < ApplicationController
   
   def index
-    @news = News.order("created_at").paginate(:per_page => per_page, :page => page)
+    @news = News.order("created_at desc").paginate(:per_page => per_page, :page => page)
   end
   
   def show
